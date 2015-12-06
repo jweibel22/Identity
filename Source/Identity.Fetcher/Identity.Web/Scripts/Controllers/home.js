2689a1818a5a89155124ba690ccff5dbcc9cd399
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2015-12-06 13:15:52 +0100

    fix: youtube videos not showing on home screen

--- Source/Identity.Fetcher/Identity.Web/Scripts/Controllers/home.js
+++ Source/Identity.Fetcher/Identity.Web/Scripts/Controllers/home.js
@@ -1,11 +1,17 @@
 ﻿angular.module('inspire')
-    .controller('HomeController', ['$scope', '$http', '$stateParams', 'homePromise','userPromise',
-        function ($scope, $http, $stateParams, homePromise, userPromise) {
+    .controller('HomeController', ['$scope', '$http', '$sce', '$stateParams', 'homePromise', 'userPromise',
+        function ($scope, $http, $sce, $stateParams, homePromise, userPromise) {
 
             $scope.contents = homePromise.data;
             $scope.user = userPromise.data;
 
             for (var i = 0; i < $scope.contents.TagCloud.length; i++) {
                 $scope.contents.TagCloud[i].link = "#/search?query=" + $scope.contents.TagCloud[i].text;
+            }
+
+            for (var i = 0; i < $scope.contents.Posts.length; i++) {
+                if ($scope.contents.Posts[i].EmbeddedUrl) {
+                    $scope.contents.Posts[i].TrustedEmbeddedUrl = $sce.trustAsResourceUrl($scope.contents.Posts[i].EmbeddedUrl);
+                }
             }
         }]);
