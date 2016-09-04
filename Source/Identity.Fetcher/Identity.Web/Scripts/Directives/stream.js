b8dc969c0f3041d5e36e14a2feebdd259ec45018
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-09-04 11:56:16 +0200

    trying to fix the problems with fetching some inline articles

--- Source/Identity.Fetcher/Identity.Web/Scripts/Directives/stream.js
+++ Source/Identity.Fetcher/Identity.Web/Scripts/Directives/stream.js
@@ -223,9 +223,21 @@
                     }
                 }
 
+                $scope.strip_tags = function(input, allowed) {
+                    allowed = (((allowed || '') + '')
+                            .toLowerCase()
+                            .match(/<[a-z][a-z0-9]*>/g) || [])
+                            .join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+                    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+                    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+                          return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+                      });
+                }
+
                 $scope.fetchPostContent = function (post) {
                     postService.fetchPostContent(post).then(function (data) {
-                        post.Description = data;
+                        var stripped = $scope.strip_tags(data, '<a><br><p><div><script>')
+                        post.Description = $sce.trustAsHtml(stripped); //data;
                     });
                 };
 
