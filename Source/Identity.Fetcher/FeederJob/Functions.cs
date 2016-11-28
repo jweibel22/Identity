73883f6989267a889f400d2dc917de03095b239f
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-11-28 06:52:27 +0100

    area

--- Source/Identity.Fetcher/FeederJob/Functions.cs
+++ Source/Identity.Fetcher/FeederJob/Functions.cs
@@ -19,5 +19,42 @@ namespace FeederJob
     public class Functions
     {
         private static ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private const string rssFeederUsername = "rssfeeder";
+
+        public static void SyncFeeds([QueueTrigger("sync-feeds")] string message, TextWriter log)
+        {
+            try
+            {
+                var connectionFactory = new ConnectionFactory(ConfigurationManager.ConnectionStrings["Sql.ConnectionString"].ConnectionString);
+
+                Identity.Domain.User rssFeederUser;
+                IEnumerable<Feed> feeders;
+
+                using (var session = connectionFactory.NewTransaction())
+                {
+                    var channelRepo = new ChannelRepository(session.Transaction);
+                    var userRepo = new UserRepository(session.Transaction);
+
+                    rssFeederUser = userRepo.FindByName(rssFeederUsername);
+                    feeders = channelRepo.OutOfSyncFeeds(TimeSpan.FromHours(1));
+                }
+
+                if (rssFeederUser == null)
+                {
+                    logger.Error("User 'rssFeeder' was not found");
+                    return;
+                }
+
+                var feedRefresher = new RssFeedRefresher(connectionFactory, log);
+                Console.WriteLine("Rss feeder started");
+                feedRefresher.Run(rssFeederUser, feeders);
+                Console.WriteLine("Rss feeder finished");
+            }
+            catch (Exception ex)
+            {
+                logger.Error("RSS feeder failed", ex);
+                throw;
+            }
+        }
     }
 }
