84409c21449ac8648727a8f694b8e6b1b5757e02
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-12-04 07:54:40 +0100

    trying to get azure to re-create the hour schedule for the feeder

---
+++ Source/Identity.Fetcher/FeederJob2/Functions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Identity.Domain;
+using Identity.Infrastructure;
+using Identity.Infrastructure.Repositories;
+using Identity.Infrastructure.Rss;
+using log4net;
+using Microsoft.Azure.WebJobs;
+using User = Identity.Infrastructure.DTO.User;
+
+namespace FeederJob2
+{
+    public class Functions
+    {
+        private static ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private const string rssFeederUsername = "rssfeeder";
+
+        public static void SyncFeeds([QueueTrigger("sync-feeds")] string message, TextWriter log)
+        {
+            try
+            {
+                var connectionFactory = new ConnectionFactory(ConfigurationManager.ConnectionStrings["Sql.ConnectionString"].ConnectionString);
+
+                Identity.Domain.User rssFeederUser;
+                IEnumerable<Feed> feeders;
+
+                using (var session = connectionFactory.NewTransaction())
+                {
+                    var channelRepo = new ChannelRepository(session.Transaction);
+                    var userRepo = new UserRepository(session.Transaction);
+
+                    rssFeederUser = userRepo.FindByName(rssFeederUsername);
+                    feeders = channelRepo.OutOfSyncFeeds(TimeSpan.FromHours(1));
+                }
+
+                if (rssFeederUser == null)
+                {
+                    logger.Error("User 'rssFeeder' was not found");
+                    return;
+                }
+
+                var feedRefresher = new RssFeedRefresher(connectionFactory, log);
+                Console.WriteLine("Rss feeder started");
+                feedRefresher.Run(rssFeederUser, feeders);
+                Console.WriteLine("Rss feeder finished");
+            }
+            catch (Exception ex)
+            {
+                logger.Error("RSS feeder failed", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ Source/Identity.Fetcher/FeederJob2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+using Microsoft.Azure.WebJobs;
+
+namespace FeederJob2
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            XmlConfigurator.Configure();
+
+            //var host = new JobHost();            
+            //host.RunAndBlock();
+
+            Functions.SyncFeeds("", TextWriter.Null);
+
+            Console.ReadLine();
+        }
+    }
+}
