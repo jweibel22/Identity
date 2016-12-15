244f178e36a4d67cd7f975c46cf74c7e15d9f5df
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-12-15 08:10:25 +0100

    fix: channels appeared empty when there were fewer posts than PageSize

--- Source/Identity.Fetcher/Identity.Infrastructure/Scripts/FetchPostIds.sql
+++ Source/Identity.Fetcher/Identity.Infrastructure/Scripts/FetchPostIds.sql
@@ -12,5 +12,5 @@ drop table IF EXISTS #MyTempTable
 select RowNum, PostId, PostClusterId into #MyTempTable from [ftFetchPosts](@ChannelId,@UserId,@OrderByColumn) where RowNum BETWEEN (@FromIndex) AND (@FromIndex+150) order by RowNum
 DECLARE @MaxRow int
 set @MaxRow = (select Min(RowNum) from #MyTempTable where PostClusterId IS NULL and RowNum >= @FromIndex+@PageSize)
-select PostId from #MyTempTable where RowNum BETWEEN (@FromIndex) AND (@MaxRow) order by RowNum
+select PostId, RowNum from #MyTempTable where RowNum BETWEEN (@FromIndex) AND (CASE WHEN @MaxRow IS NULL THEN @FromIndex+@PageSize ELSE @MaxRow END) order by RowNum
 end
