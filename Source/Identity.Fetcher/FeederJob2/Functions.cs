3aa9597ec300e6b2fa3212672cf156f5bbc4ba57
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-12-17 16:49:41 +0100

    aasfa

--- Source/Identity.Fetcher/FeederJob2/Functions.cs
+++ Source/Identity.Fetcher/FeederJob2/Functions.cs
@@ -109,7 +109,7 @@ namespace FeederJob2
 
                     articles = articles.Where(a => DistanceMeasure.Magnitude(a.WordVector) > 0).ToList();
 
-                    if (true)
+                    if (reset)
                     {
                         clusters = new List<Cluster>();
                         newArticles = articles;
